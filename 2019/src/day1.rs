922582a13bb56c819a72317b434c06604ee6dadd
Akshay Gupta <kitallis@gmail.com>
2019-12-11 19:01:22 +0530

    Solution to day1 (both parts)

--- 2019/src/day1.rs
+++ 2019/src/day1.rs
@@ -1,16 +1,71 @@
-use std::fs::File;
-use std::io::BufRead;
-use std::io::BufReader;
+use std::fs;
 
 static PUZZLE_INPUT: &str = "2019/input/day1";
 
-fn main() {
-    println!("Hello, world!");
-    let file = File::open(PUZZLE_INPUT).unwrap();
-    let reader = BufReader::new(file);
+fn fuel(mass: u64) -> u64 {
+    (mass / 3).checked_sub(2).unwrap_or(0)
+}
 
-    for (index, line) in reader.lines().enumerate() {
-        let line = line.unwrap();
-        println!("{}. {}", index + 1, line);
+fn fuel_of_fuel(mass: u64) -> u64 {
+    let cost_of_fuel: u64 = fuel(mass);
+
+    if cost_of_fuel == 0 {
+        return cost_of_fuel;
+    }
+
+    cost_of_fuel + fuel_of_fuel(cost_of_fuel)
+}
+
+fn main() {
+    let total_fuel_requirement =
+        fs::read_to_string(PUZZLE_INPUT)
+            .unwrap()
+            .lines()
+            .fold(0, |aggregate: u64, line|
+                aggregate + fuel(line.parse().unwrap()),
+            );
+
+    let total_fuel_of_fuel_requirement =
+        fs::read_to_string(PUZZLE_INPUT)
+            .unwrap()
+            .lines()
+            .fold(0, |aggregate: u64, line|
+                aggregate + fuel_of_fuel(line.parse().unwrap()),
+            );
+
+
+    println!("{}, {}", total_fuel_requirement, total_fuel_of_fuel_requirement)
+}
+
+#[cfg(test)]
+mod day_1_tests {
+    #[test]
+    fn test_1() {
+        assert_eq!(2, super::fuel(12));
+    }
+
+    #[test]
+    fn test_2() {
+        assert_eq!(2, super::fuel(14));
+    }
+
+    #[test]
+    fn test_3() {
+        assert_eq!(654, super::fuel(1969));
+    }
+
+    #[test]
+    fn test_4() {
+        assert_eq!(33583, super::fuel(100756));
+    }
+
+    #[test]
+    fn test_5() {
+        assert_eq!(966, super::fuel_of_fuel(1969));
+    }
+
+    #[test]
+    fn test_6() {
+        assert_eq!(50346, super::fuel_of_fuel(100756));
     }
 }
