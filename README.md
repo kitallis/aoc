6b9166266e43f3fece55a92e2364e309a881ae57
Akshay Gupta <kitallis@gmail.com>
2019-12-11 12:22:34 +0530

    Initial commit

---
+++ README.md
@@ -0,0 +1,28 @@
+# Advent of Code 2019
+
+# Solutions
+
+[day1.rs](src/day1.rs)
+
+# Running
+
+## Install
+
+Install Rust 2018 edition (if you don't have it already)
+
+```
+curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh
+```
+
+## Build
+
+```
+cargo build
+```
+
+## Run
+
+Run solution for a particular day
+```
+cargo run --bin day1
+```
---
+++ src/day1.rs
@@ -0,0 +1,16 @@
+use std::fs::File;
+use std::io::BufRead;
+use std::io::BufReader;
+
+static PUZZLE_INPUT: &str = "input/day1";
+
+fn main() {
+    println!("Hello, world!");
+    let file = File::open(PUZZLE_INPUT).unwrap();
+    let reader = BufReader::new(file);
+
+    for (index, line) in reader.lines().enumerate() {
+        let line = line.unwrap();
+        println!("{}. {}", index + 1, line);
+    }
+}
